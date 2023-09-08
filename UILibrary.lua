e4f943799a2c9ac4be407b52a8bcd5817b0bdcfc
AyosOwner <144406125+AyosOwner@users.noreply.github.com>
2023-09-07 21:19:46 -0700

    Update UILibrary.lua

--- UILibrary.lua
+++ UILibrary.lua
@@ -104,8 +104,8 @@ end
       SecondaryText = Color3.fromRGB(122, 122, 122),
       TertiaryText = Color3.fromRGB(158, 158, 158),
       Hovering = Color3.fromRGB(56, 53, 56),
-      Accent = Color3.fromRGB(100, 190, 31),
-      DarkerAccent = Color3.fromRGB(87, 167, 26),
+      Accent = Color3.fromRGB(133, 113, 255),
+      DarkerAccent = Color3.fromRGB(112, 83, 255),
       AccentText = Color3.fromRGB(235, 235, 235)
   }
   --//
